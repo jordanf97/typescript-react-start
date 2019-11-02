511d24af56a4ecb11ac0f1c82ad48131d20779af
Jordan Fischer <jordanfischer97@gmail.com>
2019-11-02 11:58:25 +1000

    - Change app component to simple landing page

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,14 @@ import React from 'react';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <div className="w-full max-w-md mx-auto my-5">
-        <form action="" className="bg-white shadow-md rounded px-8 py-8 pt-8">
-          <div className="px-4 pb-4">
-            <label htmlFor="email" className="text-sm block font-bold  pb-2">EMAIL ADDRESS</label>
-            <input type="email" name="email" id="" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 " placeholder="Johnbull@example.com"/>
-          </div>
-          <div  className="px-4 pb-4">
-            <label htmlFor="password" className="text-sm block font-bold pb-2">PASSWORD</label>
-            <input type="password" name="email" id="" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300" placeholder="Enter your password"/>
-          </div>
-          <div>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Sign In</button>
-          </div>
-        </form>
+    <div className={"App"}>
+      <div className={"w-full max-w-md mx-auto my-5"}>
+        <div className={"bg-white shadow-md rounded px-12 py-8 pt-8"}>
+          <h1 className={"text-center text-2xl mb-5"}> Base TypeScript React App </h1>
+          <p className={"text-center"}>
+            This is a base TypeScript React app utilizing tailwindcss, postcss and purgecss.
+          </p>
+        </div>
       </div>
     </div>
   );
